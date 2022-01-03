f88a3d6f2718afe80b13126e21bc9ae94451b00a
Pradeep <kspradeepbe@gmail.com>
2022-01-03 18:50:01 +0800

    Add prettier dev dependency package and its configuration

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    bracketSpacing: false,
+    jsxBracketSameLine: true,
+    singleQuote: true,
+    printWidth: 120,
+    tabWidth: 4,
+    semi: true,
+    bracketSpacing: true,
+    trailingComma: 'all',
+};

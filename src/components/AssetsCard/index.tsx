0a318c0aef2be59e1f53c9e00e51d83ac15004d3
Pradeep <kspradeepbe@gmail.com>
2022-01-03 23:33:49 +0800

    Add assets card to display the results of calculation

---
+++ src/components/AssetsCard/index.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import FormatUtils from '../../utils';
+import './style.scss';
+
+type Props = {
+    /**
+     * Logo icon to display in card header
+     */
+    headerIcon?: React.SVGProps<SVGSVGElement>;
+
+    /**
+     * Heading label
+     */
+    headingLabel: string;
+    
+    /**
+     * Use selected currency symbol
+     */
+    selecteCurrency: string;
+
+    selectedCurrencyRatePerCoin: undefined | number;
+
+    /**
+     * Total no of masternodes available
+     */
+    noOfMasterNodes?: number;
+    
+    /**
+     * Total no of masternodes available
+     */
+    noOfCoins?: number;
+    
+    /**
+     * Total no of masternodes available
+     */
+    totalValueOfAssets?: number;
+}
+
+const AssetsUnderManagementCard: React.FC<Props> = ({ headerIcon, headingLabel, selecteCurrency, selectedCurrencyRatePerCoin, totalValueOfAssets, noOfCoins: noOfActiveMasterNodes, noOfMasterNodes }) => {
+
+    return (
+        <div className='card'>
+            <div className='card--header'>
+                { headerIcon && headerIcon}
+                <h3>{headingLabel}</h3>
+                <h4>{selectedCurrencyRatePerCoin && `${selectedCurrencyRatePerCoin} ${selecteCurrency}`}</h4>
+            </div>
+            <div className='card--content'>
+                <div className='info'>
+                    <label>Total masternodes</label>
+                    <p>{FormatUtils.formatNumber(noOfMasterNodes)}</p>
+                </div>
+                <div className='info'>
+                    <label>Total Coins</label>
+                    <p>{FormatUtils.formatNumber(noOfActiveMasterNodes)}</p>
+                </div>
+                <div className='info'>
+                    <label>Total value in</label>
+                    <p>{FormatUtils.formatCurrency(totalValueOfAssets, selecteCurrency)}</p>
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default AssetsUnderManagementCard;
